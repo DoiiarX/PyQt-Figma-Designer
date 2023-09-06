3bc7ed4998c48eba40189b915131e93af1f38022
rombirli <romain.birling@epfl.ch>
2023-09-06 17:15:46 +0200

    UTF-8 encoding crash fixed

--- tests/test_full.py
+++ tests/test_full.py
@@ -7,6 +7,6 @@ test_read_figma.run()
 with open('../resources/f.fig', 'rb') as file:
     figma_file = pickle.load(file)
 python_code = '\n'.join(pyqt_generator.generate_pyqt_design(figma_file))
-with open('../resources/f.py', 'w') as file:
+with open('../resources/f.py', 'w', encoding="utf-8") as file:
     file.write(python_code)
 exec(python_code)
