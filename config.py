f89ea35b909619b198df9402051cb896f66222c9
rombirli <romain.birling@epfl.ch>
2023-09-07 16:25:47 +0200

    Started putting images into svg(s)

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import re
 
 token = 'figd_-QMtU_8nZoTs48qAzWTqQecktPRyh3gCI9rR0Jx5'
 file_url = 'https://www.figma.com/file/DEtNOgq9OGnkGPfPpiQeEK/Untitled?type=design&mode=design&t=MXkH4iNj09GBg1aG-0'
-# 'https://www.figma.com/file/PExygXiMSTBEpXXEq0ZPyE/Untitled?type=design&node-id=0-1&mode=design&t=D4pN9YixWIbPDYlb-0'
+# file_url = 'https://www.figma.com/file/PExygXiMSTBEpXXEq0ZPyE/Untitled?type=design&node-id=0-1&mode=design&t=D4pN9YixWIbPDYlb-0'
 
 match = re.search(r'https://www.figma.com/file/([0-9A-Za-z]+)', file_url.strip())
 file_key = match.group(1).strip()
