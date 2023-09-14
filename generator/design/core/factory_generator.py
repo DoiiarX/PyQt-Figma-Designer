f21a81f8ee88d58cfc052a8cedf81985aa8a028b
rombirli <romain.birling@epfl.ch>
2023-09-14 03:40:42 +0200

    Generic instantiation in factory_generator.py (from components module)

--- generator/design/core/factory_generator.py
+++ generator/design/core/factory_generator.py
@@ -1,21 +1,14 @@
+import importlib.util
 import inspect
+import os
 
 from generator.design.component_generator import ComponentGenerator
-from generator.design.components.custom_button_generator import CustomButtonGenerator
-from generator.design.components.custom_text_field_generator import CustomTextFieldGenerator
-from generator.design.components.progress_bar_generator import ProgressBarGenerator
-from generator.design.components.button_generator import ButtonGenerator
-from generator.design.components.checkbox_generator import CheckboxGenerator
-from generator.design.components.slider_generator import SliderGenerator
-from generator.design.components.tabs_view_generator import TabsViewGenerator
-from generator.design.components.text_field_generator import TextFieldGenerator
-from generator.design.design_generator import DesignGenerator
 from generator.design.core.group_generator import GroupGenerator
 from generator.design.core.text_generator import TextGenerator
 from generator.design.core.vector_generator import VectorGenerator
+from generator.design.design_generator import DesignGenerator
 from generator.properties.visibility_generator import VisibilityGenerator
 from generator.utils import generate_q_widget
-import generator.design.components as components
 
 
 class FactoryGenerator(DesignGenerator):
@@ -40,18 +33,28 @@ class FactoryGenerator(DesignGenerator):
             yield from group_generator.generate_design()
 
         # generate inputs
-        name = self.figma_node['name'].lower().replace(' ', '').replace('-', '').replace('_', '')
 
         if group_generator is not None:
-            # enumerate classes in module design.components
-            for _, obj in inspect.getmembers(components, inspect.ismodule):
-                for _, cls in inspect.getmembers(obj, inspect.isclass):
-                    # if cls inherits ComponentGenerator
-                    if issubclass(cls, ComponentGenerator) and cls != ComponentGenerator:
-                        prefix_rule = cls.prefix_rule.lower().replace(' ', '').replace('-', '').replace('_', '')
-                        if name.startswith(prefix_rule):
-                            yield from cls(self.figma_node, self, group_generator).generate_design()
+            yield from self.generate_generic_components(group_generator)
 
         # hide the component if it is not visible
         if not self.figma_node.get('visible', True):
             yield from VisibilityGenerator(self).generate_set('False')
+
+    def generate_generic_components(self, group_generator):
+        name = self.figma_node['name'].lower().replace(' ', '').replace('-', '').replace('_', '')
+        module_directory = 'generator/design/components'
+        module_files = [f for f in os.listdir(module_directory) if f.endswith('.py')]
+        # Remove the file extension to get module names.
+        module_names = [os.path.splitext(f)[0] for f in module_files]
+        # Import the modules and list them.
+        for module_name in module_names:
+            spec = importlib.util.spec_from_file_location(module_name,
+                                                          os.path.join(module_directory, module_name + '.py'))
+            module = importlib.util.module_from_spec(spec)
+            spec.loader.exec_module(module)
+            for _, cls in inspect.getmembers(module, inspect.isclass):
+                if issubclass(cls, ComponentGenerator) and cls != ComponentGenerator:
+                    prefix_rule = cls.prefix_rule.lower().replace(' ', '').replace('-', '').replace('_', '')
+                    if name.startswith(prefix_rule):
+                        yield from cls(self.figma_node, self, group_generator).generate_design()
